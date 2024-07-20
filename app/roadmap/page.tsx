f9fd3e94a68365fd6e42e27e77c0680572b234f3
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-07-20 19:18:33 +0200

    changes

---
+++ app/roadmap/page.tsx
@@ -0,0 +1,72 @@
+export default function Roadmap() {
+    return (
+        <div className=" min-vh-100 text-center justify-content-center d-flex align-items-center">
+            <div>
+
+                <div className="mb-3">
+                    <h1 className="display-1 fw-bold">
+                        ROADMAP
+                    </h1>
+                    <p>Building Tools With A <b className="p-text">Purpose</b></p>
+                </div>
+                <div className="row gap-3 w-100 mb-3">
+                    <div className="col-sm w-100 d-flex align-items-center">
+                        <div>
+                        <div className="pt-2">
+                        <p><i className="bi bi-circle-fill text-primary "></i></p>
+                        </div>
+                        <div className="border rounded p-2">
+                            <div>
+                            <h5>i-Claim</h5>
+                            <p>
+                            We are enablling health providers to receive timely payments by assisting insurance companies in swiftly detecting fraud and errors in claims, reducing the waiting period from 30 days to just seconds.
+                            </p>
+                            </div>
+                        </div>
+                        </div>
+                        
+                    </div>
+                    
+                    <div className="col-sm w-100 d-flex align-items-center">
+                        <div>
+                        <div className="pt-2">
+                        <p><i className="bi bi-circle-fill text-primary "></i></p>
+                        </div>
+                        <div className="border rounded p-2">
+                        <div>
+                            <h5>Bleu</h5>
+                            <p>
+                            We are redefining banking across Africa by creating a connected network that enables instant payments. Our neobank will provide financial access to all, making banking accessible throughout the continent.
+                            </p>
+                            </div>
+                        </div>
+                        </div>
+                       
+                    </div>
+                    <div className="col-sm w-100 d-flex align-items-center">
+                        <div>
+
+                        
+                        <div className="pt-2">
+                        <p><i className="bi bi-circle-fill text-primary "></i></p>
+                        </div>
+                        <div className="border rounded p-2">
+                        <div>
+                            <h5>Attribute AI</h5>
+                            <p>
+                            We are building a safer alternative to current AI platforms, Attribute will provide sources and references for responses given making sure you can trust and give credit to those who deserve it
+                            </p>
+                            </div>
+                        </div>
+                        </div>
+                        
+                    </div>
+                   
+                </div>
+                <div>
+                    <button className="btn p-btn">Stay Updated</button>
+                </div>
+            </div>
+        </div>
+    )
+}
