a4d47a82dd2f95389f27c7e0d87e5b8cb1872b27
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-07-21 18:59:22 +0200

    changes

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -1,22 +1,27 @@
+import Image from "next/image"
 export default function Roadmap() {
     return (
         <div className=" min-vh-100 text-center justify-content-center d-flex align-items-center">
             <div>
 
-                <div className="mb-3">
+                <div className="mb-3 text-center">
                     <h1 className="display-1 fw-bold">
                         ROADMAP
                     </h1>
                     <p>Building Tools With A <b className="p-text">Purpose</b></p>
                 </div>
-                <div className="row gap-3 w-100 mb-3">
-                    <div className="col-sm w-100 d-flex align-items-center">
+                <div>
+                    <Image src="https://ngratesc.sirv.com/website/roadmap-line.png" className="img-fluid" width={400} height={0} alt="Line showing roadmap"/>
+                </div>
+                <div className=" mb-3 roadmap_container gap-3 vw-100">
+                    <div className="d-flex align-items-center roadmap_card">
                         <div>
                         <div className="pt-2">
                         <p><i className="bi bi-circle-fill text-primary "></i></p>
                         </div>
                         <div className="border rounded p-2">
                             <div>
+                                <p className="p-text fw-normal">Q4 -2024</p>
                             <h5>i-Claim</h5>
                             <p>
                             We are enablling health providers to receive timely payments by assisting insurance companies in swiftly detecting fraud and errors in claims, reducing the waiting period from 30 days to just seconds.
@@ -27,13 +32,15 @@ export default function Roadmap() {
                         
                     </div>
                     
-                    <div className="col-sm w-100 d-flex align-items-center">
+                    <div className="d-flex align-items-center roadmap_card">
                         <div>
                         <div className="pt-2">
                         <p><i className="bi bi-circle-fill text-primary "></i></p>
                         </div>
                         <div className="border rounded p-2">
                         <div>
+                        <p className="p-text fw-normal">Q4 -2025</p>
+
                             <h5>Bleu</h5>
                             <p>
                             We are redefining banking across Africa by creating a connected network that enables instant payments. Our neobank will provide financial access to all, making banking accessible throughout the continent.
@@ -43,7 +50,7 @@ export default function Roadmap() {
                         </div>
                        
                     </div>
-                    <div className="col-sm w-100 d-flex align-items-center">
+                    <div className="d-flex align-items-center roadmap_card">
                         <div>
 
                         
@@ -52,9 +59,11 @@ export default function Roadmap() {
                         </div>
                         <div className="border rounded p-2">
                         <div>
+                        <p className="p-text fw-normal">Q2 -2025</p>
+
                             <h5>Attribute AI</h5>
                             <p>
-                            We are building a safer alternative to current AI platforms, Attribute will provide sources and references for responses given making sure you can trust and give credit to those who deserve it
+                            We are building a safer alternative to current AI platforms, Attribute will provide sources and references for responses given making sure you can trust and give credit to those who deserve it. AI which is truly Open Source
                             </p>
                             </div>
                         </div>
