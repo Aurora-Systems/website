5639dce1aa96af1b0bd647667af49dcbc8a2104a
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-08-03 18:32:17 +0200

    changes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,8 +4,8 @@ export default function Blog(){
         <div className="min-vh-100 mt-3">
           <div className="text-center">
           
-            <div className="">
-            <iframe src="https://aurorasystems.substack.com/embed" width="480" height="320" style={{border:"1px solid #EEE;", background:"white;"}} frameBorder="0" scrolling="no"></iframe>
+            <div className="container">
+            <iframe src="https://aurorasystems.substack.com/embed" width="380" height="320" style={{border:"1px solid #EEE;", background:"white;"}} frameBorder="0" scrolling="no"></iframe>
             </div>
           </div>
         <SubstackFeed/>
