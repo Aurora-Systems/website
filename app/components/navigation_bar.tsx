9543d9f8fd81e23f3652032155d47a324a0f719a
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-08-13 18:44:17 +0200

    fixed navbar

--- app/components/navigation_bar.tsx
+++ app/components/navigation_bar.tsx
@@ -3,80 +3,157 @@ import Link from 'next/link';
 import page_links from './links';
 import Image from "next/image"
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const router = useRouter();
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
     };
 
+    const closeNavbar = () => {
+        setIsOpen(false);
+    };
+
+    const handleNavigation = (url:string) => {
+        router.push(url);
+        closeNavbar();
+    };
+
     return (
         <nav className="bg-white rounded mt-3 z-1 navbar navbar-expand-lg bg-white position-sticky top-0 pt-3 w-100 ">
             <div className="container">
-                <Link href="/"><h5 className="mt-1 me-3" >Aurora</h5></Link>
+                <Link href="/" onClick={closeNavbar}><h5 className="mt-1 me-3" >Aurora</h5></Link>
                 <button className="navbar-toggler bg-white border-0" type="button" onClick={toggleNavbar}>
                     <i className="bi bi-list text-primary"></i>
                 </button>
                 
                 <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
-               
-                            
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                       <li className='nav-item'>
-                    <div className='dropdown '>
+                        <li className='nav-item'>
+                            <div className='dropdown'>
                                 <button type="button" className='bg-white btn-none border-0 ms-0 nav-link' data-bs-toggle="dropdown" aria-expanded="false">Services <i className="bi bi-chevron-down"></i></button>
-                               
-                                <ul className='dropdown-menu '>
-                               
-                                <li>
-                                        <Link className='dropdown-item' href={page_links.services.custom_dev}>Custom Software Development</Link>
+                                <ul className='dropdown-menu'>
+                                    <li className='pointer'>
+                                        <a className='dropdown-item' onClick={() => handleNavigation(page_links.services.custom_dev)}>Custom Software Development</a>
                                     </li>
-                                    <li>
-                                        <Link className='dropdown-item' href={page_links.services.ux}>User Interface Design UI | UX</Link>
+                                    <li  className='pointer'>
+                                        <a className='dropdown-item' onClick={() => handleNavigation(page_links.services.ux)}>User Interface Design UI | UX</a>
                                     </li>
-                                    <li>
-                                        <Link className='dropdown-item' href={page_links.services.mobile_dev}>Mobile App Development</Link>
+                                    <li  className='pointer'>
+                                        <a className='dropdown-item' onClick={() => handleNavigation(page_links.services.mobile_dev)}>Mobile App Development</a>
                                     </li>
-                                    
-                                    <li>
-                                        <Link className='dropdown-item' href={page_links.services.ai}>AI and Automation</Link>
+                                    <li  className='pointer'>
+                                        <a className='dropdown-item' onClick={() => handleNavigation(page_links.services.ai)}>AI and Automation</a>
                                     </li>
-                                    <li>
-                                        <Link className='dropdown-item' href={page_links.services.web_dev}>Web Development</Link>
+                                    <li  className='pointer'>
+                                        <a className='dropdown-item' onClick={() => handleNavigation(page_links.services.web_dev)}>Web Development</a>
                                     </li>
-                                    <li>
-                                        <Link className='dropdown-item' href={page_links.services.idea_validation}>Idea Validation</Link>
+                                    <li  className='pointer'>
+                                        <a className='dropdown-item' onClick={() => handleNavigation(page_links.services.idea_validation)}>Idea Validation</a>
                                     </li>
-                                   
-                                   
                                 </ul>
                             </div>
-                            </li>
-                       
-                        <li className="nav-item">
-                            <Link className="nav-link" href={page_links.roadmap}>Roadmap</Link> 
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href={page_links.portfolio}>Portfolio</Link>
+                            <Link className="nav-link" href={page_links.roadmap} onClick={closeNavbar}>Roadmap</Link> 
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href={page_links.careers}>Careers</Link>
+                            <Link className="nav-link" href={page_links.portfolio} onClick={closeNavbar}>Portfolio</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href={page_links.blog}>Blog</Link>
+                            <Link className="nav-link" href={page_links.careers} onClick={closeNavbar}>Careers</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href={page_links.contact}>Contact Us</Link>
+                            <Link className="nav-link" href={page_links.blog} onClick={closeNavbar}>Blog</Link>
                         </li>
-                        
+                        <li className="nav-item">
+                            <Link className="nav-link" href={page_links.contact} onClick={closeNavbar}>Contact Us</Link>
+                        </li>
                     </ul>
-                    <Link href={page_links.contact}><button className="btn p-btn rounded w-sm-100">Ready To Build?</button></Link>
+                    <Link href={page_links.contact} onClick={closeNavbar}><button className="btn p-btn rounded w-sm-100">Ready To Build?</button></Link>
                 </div>
             </div>
         </nav>
     );
 };
+
+// export const Navbar = () => {
+//     const [isOpen, setIsOpen] = useState(false);
+
+//     const toggleNavbar = () => {
+//         setIsOpen(!isOpen);
+//     };
+
+//     return (
+//         <nav className="bg-white rounded mt-3 z-1 navbar navbar-expand-lg bg-white position-sticky top-0 pt-3 w-100 ">
+//             <div className="container">
+//                 <Link href="/"><h5 className="mt-1 me-3" >Aurora</h5></Link>
+//                 <button className="navbar-toggler bg-white border-0" type="button" onClick={toggleNavbar}>
+//                     <i className="bi bi-list text-primary"></i>
+//                 </button>
+                
+//                 <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
+               
+                            
+//                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                        <li className='nav-item'>
+//                     <div className='dropdown '>
+//                                 <button type="button" className='bg-white btn-none border-0 ms-0 nav-link' data-bs-toggle="dropdown" aria-expanded="false">Services <i className="bi bi-chevron-down"></i></button>
+                               
+//                                 <ul className='dropdown-menu '>
+                               
+//                                 <li>
+//                                         <Link className='dropdown-item' data-bs-toggle="collapse" href={page_links.services.custom_dev}>Custom Software Development</Link>
+//                                     </li>
+//                                     <li>
+//                                         <Link className='dropdown-item' href={page_links.services.ux}>User Interface Design UI | UX</Link>
+//                                     </li>
+//                                     <li>
+//                                         <Link className='dropdown-item' href={page_links.services.mobile_dev}>Mobile App Development</Link>
+//                                     </li>
+                                    
+//                                     <li>
+//                                         <Link className='dropdown-item' href={page_links.services.ai}>AI and Automation</Link>
+//                                     </li>
+//                                     <li>
+//                                         <Link className='dropdown-item' href={page_links.services.web_dev}>Web Development</Link>
+//                                     </li>
+//                                     <li>
+//                                         <Link className='dropdown-item' href={page_links.services.idea_validation}>Idea Validation</Link>
+//                                     </li>
+                                   
+                                   
+//                                 </ul>
+//                             </div>
+//                             </li>
+                       
+//                         <li className="nav-item">
+//                             <Link className="nav-link" href={page_links.roadmap}>Roadmap</Link> 
+//                         </li>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" href={page_links.portfolio}>Portfolio</Link>
+//                         </li>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" href={page_links.careers}>Careers</Link>
+//                         </li>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" href={page_links.blog}>Blog</Link>
+//                         </li>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" href={page_links.contact}>Contact Us</Link>
+//                         </li>
+                        
+//                     </ul>
+//                     <Link href={page_links.contact}><button className="btn p-btn rounded w-sm-100">Ready To Build?</button></Link>
+//                 </div>
+//             </div>
+//         </nav>
+//     );
+// };
 
 
 export const Footer=()=>{
