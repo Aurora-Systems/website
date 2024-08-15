10023c7f55a9240cad624be9e550333a1aea5772
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-08-15 19:44:16 +0200

    fixed contact page

--- app/contact_us/page.tsx
+++ app/contact_us/page.tsx
@@ -1,4 +1,24 @@
+"use client"
+import { FormEvent, useRef, useState } from "react"
+import emailjs from "@emailjs/browser"
+
 export default function ContactUs(){
+    const [loading,set_loading] = useState<boolean>(false)
+    const form: any = useRef()
+    const send_application = (e: FormEvent) => {
+        e.preventDefault()
+        set_loading(true)
+        emailjs.sendForm("service_l2ay0f2", "template_c4pjfnx", form.current, {
+            publicKey: "CLt7ZGjfiSQEopImB"
+        }).then(() => {
+            alert("✅ We received your enquiry, expect a call or an email soon!")
+            form.current.reset()
+        }).catch(() => {
+            alert("⚠️ Message not sent, please try again or send us a message on our email support@aurorasystems.co.zw!")
+        }).finally(()=>{
+            set_loading(false)
+        })
+    }
     return(
     <div className=" min-vh-100">
         <div className="text-center mt-5">
@@ -6,35 +26,35 @@ export default function ContactUs(){
             <p>Get on a quick 30 min call with us!</p>
         </div>
         <div className="bg-light rounded mb-3 p-3">
-            <form >
+            <form ref={form} onSubmit={send_application}>
                 <div className="row">
                     <div className="col-sm mb-2">
                         <span>Name</span>
-                        <input type="text" className="form-control" name="" placeholder=""/>
+                        <input type="text" className="form-control" name="full_name" placeholder=""/>
                     </div>
                     <div className="col-sm mb-2">
                         <span>Company Name</span>
-                        <input type="text" className="form-control" name="" placeholder=""/>
+                        <input type="text" className="form-control" name="company_name" placeholder=""/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-sm mb-2">
                         <span>Email</span>
-                        <input type="text" className="form-control" name="" placeholder=""/>
+                        <input type="text" className="form-control" name="email" placeholder=""/>
                     </div>
                     <div className="col-sm mb-2">
                         <span>Contact Number</span>
-                        <input type="text" className="form-control" name="" placeholder=""/>
+                        <input type="text" className="form-control" name="contact_number" placeholder=""/>
                     </div>
                 </div>
                 <div className="row">
                 <div className="col-sm mb-2">
                         <span>Your Budget (USD)</span>
-                        <input type="text" className="form-control" name="" placeholder=""/>
+                        <input type="text" className="form-control" name="budget" placeholder=""/>
                     </div>
                     <div className="col-sm mb-2">
                         <span>Service Interested In?</span>
-                        <select className="form-control ">
+                        <select className="form-control " name="service">
                             <option value="AI and Automation">AI and Automation</option>
                             <option value="Web App Development">Web App Development</option>
                             <option value="Custom Software Development">Custom Software Development</option>
@@ -48,12 +68,12 @@ export default function ContactUs(){
                 <div className="row">
                     <div className="col-sm mb-2">
                         <span>Tell us more about your project</span>
-                        <textarea className="form-control"></textarea>
+                        <textarea className="form-control" name="project_details"></textarea>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-sm">
-                        <button className="btn p-btn">Send</button>
+                        <button type="submit" className="btn p-btn">Send</button>
                     </div>
                 </div>
             </form>
