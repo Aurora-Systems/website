e6faec157345146f3ed7fcec6e10e1558b4e9fe1
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-11-16 14:07:02 +0200

    ADDED HEAD

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { Metadata } from "next";
 import { League_Spartan } from "next/font/google";
 import "./globals.css";
@@ -18,6 +19,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            _linkedin_partner_id = "7844609";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `
+        }} />
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `
+        }} />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=7844609&fmt=gif" />
+        </noscript>
+      </Head>
       <body className={`${league.className} container`}>
         <Navbar/>
         {children}
