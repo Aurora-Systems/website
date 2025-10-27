869cdbcc55f730f4cc36e8a296e6d1c27cf9e4c5
Zayne <zaynekomichi@hotmail.com>
2025-10-27 10:28:58 +0200

    refactor: replace inline Zoho SalesIQ script with Next.js Script component for improved performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Navbar, Footer } from "./components/navigation_bar";
 import BootstrapClient from "./components/bootstrap_client"
 import { LinkedInInsightTag } from "nextjs-linkedin-insight-tag";
+import Script from "next/script";
 
 const league = League_Spartan({ subsets: ["latin"] });
 
@@ -43,8 +44,25 @@ export default function RootLayout({
         <LinkedInInsightTag/>
                 </div>
                         <Footer/>
-
-<script async type="text/javascript">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"siqf22096acdd69860087f120f3a2ad5f5dda6df0bd5c3334259ad020db051987f8", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
+                        <Script id="zoho-salesiq" strategy="afterInteractive">
+          {`
+            var $zoho=$zoho || {};
+            $zoho.salesiq = $zoho.salesiq || {
+              widgetcode:"siqf22096acdd69860087f120f3a2ad5f5dda6df0bd5c3334259ad020db051987f8",
+              values:{},
+              ready:function(){}
+            };
+            var d=document;
+            s=d.createElement("script");
+            s.type="text/javascript";
+            s.id="zsiqscript";
+            s.defer=true;
+            s.src="https://salesiq.zoho.com/widget";
+            t=d.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(s,t);
+          `}
+        </Script>
+{/* <script async type="text/javascript">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"siqf22096acdd69860087f120f3a2ad5f5dda6df0bd5c3334259ad020db051987f8", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script> */}
       </body>
     </html>
   );
